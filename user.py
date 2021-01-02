c9ebccae60cd46aca05904765dadd05d45ba88ad
miller2590 <59415200+miller2590@users.noreply.github.com>
2021-01-02 17:24:16 -0600

    Added delete movies method

--- user.py
+++ user.py
@@ -1,3 +1,6 @@
+from movie import Movie
+
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -5,6 +8,12 @@ class User:
 
     def __repr__(self):
         return f'User {self.name}'
+
+    def add_movie(self, name, genre):
+        self.movies.append(Movie(name, genre, False))
+
+    def delete_movie(self, name):
+        self.movies = list(filter(lambda x: x.name != name, self.movies))
 
     def watched_movies(self):
         movies_watched = list(filter(lambda x: x.watched, self.movies))
